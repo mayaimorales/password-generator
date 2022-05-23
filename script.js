233d7f30e929fd79f2996ccfa4a5237a9dae9ada
Maya Morales <mayainomorales@gmail.com>
2022-05-23 14:03:00 -0700

    fixed formatting

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var digits = [0,1,2,3,4,5,6,7,8,9];
-var specialChar = ["#","&","%","$","@","!","^","*","?"];
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialChar = ["#", "&", "%", "$", "@", "!", "^", "*", "?"];
 
 // Write password to the #password input
 function writePassword() {
@@ -20,66 +20,59 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   var passLength = window.prompt("what is the desired length of your password? (must be 8-128)");
-
   var uppercase = window.confirm("would you like to include uppercase letters?");
-
   var lowercase = window.confirm("would you like to include lowercase letters?");
-
   var numbers = window.confirm("would you like to include any numbers?");
-
   var special = window.confirm("would you like to include any special characters?");
 
-  if(uppercase===false && lowercase===false && numbers === false && special === false){
+  if (uppercase === false && lowercase === false && numbers === false && special === false) {
     alert("please try again");
   }
 
-  if(passLength < 8 || passLength > 128 || !passLength){
+  if (passLength < 8 || passLength > 128 || !passLength) {
     alert("password length must be 8-128, please try again.");
   }
+
+  var password = [];
+
+  function random(a) {
+    var i = Math.floor(Math.random * a.length);
+    return a[i];
+  };
+
+  for (var i = 0; i < password.length;) {
+    if (uppercase === true && i < password.length) {
+      var a = uppercaseLetters;
+      var newArr = random(a);
+      password.push(newArr);
+      i++;
+    }
+    if (lowercase === true && i < password.length) {
+      var a = lowercaseLetters;
+      var newArr = random(a);
+      password.push(newArr);
+      i++;
+    }
+    if (numbers === true && i < password.length) {
+      var a = digits;
+      var newArr = random(a);
+      password.push(newArr);
+      i++;
+    }
+    if (special === true && i < password.length) {
+      var a = specialChar;
+      var newArr = random(a);
+      password.push(newArr);
+      i++;
+    }
+    password = password.sort(function (a,b) { return 0.5 - Math.random() });
+    password = password.join('');
+  
+    return password;
+  
+  };
 };
 
-function random(a){
-  var i = Math.floor(Math.random*a.length);
-  return a[i];
-};
-
-var password = [];
-
-for (var i=0; i < password.length;){
-  if (uppercase === true && i < password.length){
-    var a = uppercaseLetters;
-    var newArr = random(a);
-    password.push(newArr);
-    i++;
-  }
-  if (lowercase === true && i < password.length){
-    var a = lowercaseLetters;
-    var newArr = random(a);
-    password.push(newArr);
-    i++;
-  }
-  if (numbers === true && i < password.length){
-    var a = digits;
-    var newArr = random(a);
-    password.push(newArr);
-    i++;
-  }
-  if (special === true && i < password.length){
-    var a = specialChar;
-    var newArr = random(a);
-    password.push(newArr);
-    i++;
-  }
-}
-function result (){
-  password = password.sort(function(a,b){return 0.5 - Math.random()});
-  password = password.join('');
-
-  return password;
-
-}
-
-result();
 
 
 
